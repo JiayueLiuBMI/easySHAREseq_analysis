49580e3f272b0caafb9739fd27af187be7a47950
jiayue_liu_biology <35744343+JiayueLiuBMI@users.noreply.github.com>
2025-01-15 17:18:38 -0500

    Add files via upload

--- demultiplex_trial.sh
+++ demultiplex_trial.sh
@@ -6,10 +6,12 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jiayuel@mit.edu
 
-# make sure to complile the c++ first in the Scripts Directory
+# Make sure to complile the c++ first in the Scripts Directory
 # g++ -O3 -o SHARE_demult_fastq_BC_AC_011525.o SHARE_demult_fastq_BC_AC.cpp -lgzstream -I/home/jiayuel/.local/usr/include -L/home/jiayuel/.local/lib -I/home/jiayuel/.local/usr/include -std=gnu++11 -lz -Wall
-
 # then take the directory back to where this script was located at
+
+# Create a space for debug if it doesn't exist 
+# mkdir /home/jiayuel/data/easySHAREseq_workstation/Debug/Demultiplex_trial/input
 
 module load miniconda3/v4
 
@@ -20,22 +22,23 @@ conda activate shareseq_env
 echo "Working directory: $(pwd)"
 
 # Main directory for your project
-main_dir="/home/jiayuel/data/easySHAREseq/easySHARE_seq/Demultiplexing"
+main_dir="/home/jiayuel/data/easySHAREseq_workstation/easySHARE_seq/Demultiplexing"
 echo "Main directory: $main_dir"
 
-# Create a space for debug 
-# mkdir /home/jiayuel/data/easySHAREseq_workstation/Debug/Demultiplex_trial/input
-# cp /home/jiayuel/data/30-1116768544/00_fastq/sgInert-1-sc*.fastq.gz
 
 # Input directory
-input_dir="/home/jiayuel/data/easySHAREseq/Debug/Demultiplex_trial/input/"
+input_dir="/home/jiayuel/data/easySHAREseq_workstation/Debug/Demultiplex_trial/input/"
 echo "Input directory: $input_dir"
 echo "Listing files in the input directory:"
 ls -l $input_dir
 
 # Output directory
-output_dir="/home/jiayuel/data/easySHAREseq/Debug/Demultiplex_trial/output"
+output_dir="/home/jiayuel/data/easySHAREseq_workstation/Debug/Demultiplex_trial/output"
 echo "Output directory: $output_dir"
+
+# cp the test files
+# make sure to double check if the directory is right.
+#cp /home/jiayuel/data/30-1116768544/00_fastq/sgInert-1-sc*.fastq.gz
 
 # Test run with a small batch of data (one sample in this case)
 R1_file="${input_dir}sgInert-1-sc-ATAC_R1_001.fastq.gz"
@@ -51,9 +54,15 @@ fi
 
 echo "All required input files found!"
 
+# copy barcodes and scripts from maindir
+cp $main_dir/Scripts/BC_A.txt ./
+cp $main_dir/Scripts/BC_C.txt ./
+cp $main_dir/Scripts/SHARE_demult_fastq_BC_AC_011525.o ./
+
+
 # Run the C++ executable with input and output directory paths as arguments
 echo "Running the C++ executable SHARE_demult_fastq_orig.o with input and output directories"
-./SHARE_demult_fastq_orig.o "$input_dir" "$output_dir"
+./SHARE_demult_fastq_BC_AC_011525.o "$input_dir" "$output_dir"
 
 # Output completion message
 echo "Demultiplexing process completed."
